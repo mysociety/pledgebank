ea7404c70a9ea701344606a20115427797851f75
matthew <matthew>
2005-03-04 13:19:57 +0000

    Move staging message under heading.

--- templates/page.php
+++ templates/page.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: page.php,v 1.9 2005-03-04 13:16:41 matthew Exp $
+// $Id: page.php,v 1.10 2005-03-04 13:19:57 matthew Exp $
 
 function page_header($title) { ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
@@ -14,15 +14,13 @@ function page_header($title) { ?>
 <script type="text/javascript" src="pb.js"></script>
 </head>
 <body>
-<?
-    if (OPTION_PB_STAGING) {
-        print "<i>This is a test site for developers only.  You
-        probably want <a href=\"http://www.pledgebank.com\">the real site</a>.</i>";
-    }
-?>
 <h1>Pledge<span>Bank</span> &mdash; Not Finished Yet</h1>
 <hr class="v"><div id="content">
-<? 
+<?
+    if (OPTION_PB_STAGING) {
+        print '<p align="center" style="color: #cc0000; background-color: #ffffff"><i>This is a test site for developers only.  You
+        probably want <a href="http://www.pledgebank.com/">the real site</a>.</i></p>';
+    }
 }
 
 function page_footer() { ?>

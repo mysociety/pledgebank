d9ddf9127809de28740a6ebb5d9a2ab0d068b21d
francis <francis>
2005-03-14 14:14:18 +0000

    Fixed bug in staging site display

--- phplib/page.php
+++ phplib/page.php
@@ -6,7 +6,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: page.php,v 1.4 2005-03-11 21:06:38 francis Exp $
+// $Id: page.php,v 1.5 2005-03-14 14:14:18 francis Exp $
 
 /* page_header TITLE
  * Print top part of HTML page, with the given TITLE. This prints up to the
@@ -25,11 +25,11 @@ function page_header($title) {
 <h1>Pledge<span>Bank</span> &mdash; Not Finished Yet</h1>
 <hr class="v"><div id="content"><?
 
-    if (OPTION_PB_STAGING)
+    if (OPTION_PB_STAGING) {
 ?><p align="center" style="color: #cc0000; background-color: #ffffff">
 <i>This is a test site for developers only. You probably want
 <a href="http://www.pledgebank.com/">the real site</a>.</i></p><?
-
+    }
 }
 
 /* page_footer

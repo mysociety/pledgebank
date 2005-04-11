41a1214465ae6948cf136b32ffc47a14a17f9c12
francis <francis>
2005-04-11 17:39:57 +0000

    Bits getting test suite to pass again.

--- web/confirm.php
+++ web/confirm.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: confirm.php,v 1.22 2005-04-06 13:52:59 chris Exp $
+ * $Id: confirm.php,v 1.23 2005-04-11 17:39:57 francis Exp $
  * 
  */
 
@@ -123,19 +123,35 @@ page_footer(array('nonav'=>true));
 function advertise_flyers($pledge_id) {
     $r = db_getRow('select * from pledges where id = ?', $pledge_id);
     $png_flyers8_url = new_url("/flyers/{$r['ref']}_A4_flyers8.png", false);
+
 ?>
 <p class="noprint" align="center">
 <strong>Important Notice</strong> &mdash; You will massively increase the
 chance of this pledge succeeding if you
-<?print_this_link("print this page out", ",")?>
-cut up the flyers and stick them through some of your neighbours' letterboxes.
+<? if (!$r['password']) {
+        print_this_link("print this page out", ",");
+   } else {
+        // TODO - we don't have the password raw here, but really want it on
+        // form to pass on for link to flyers page.  Not sure how best to fix
+        // this up.
+        print_link_with_password("/".htmlspecialchars($r['ref'])."/flyers", "", "print these pages out");
+   }
+?>
+ cut up the flyers and stick them through some of your neighbours' letterboxes.
 We cannot emphasise this enough &mdash; print them now and post them next time
-you go out to the shops. We also have
-<a href="/<?=htmlspecialchars($r['ref']) ?>/flyers">more attractive PDF
+you go out to the shops. 
+
+<? 
+    // Show inline graphics only for passwordless pledges (as PNG doesn't
+    // work for the password protected ones, you can't POST a password
+    // into an IMG SRC= link)
+    if (!$r['password']) { ?>
+We also have <a href="/<?=htmlspecialchars($r['ref']) ?>/flyers">more attractive PDF
 versions</a>.</p>
 
 <p align="center"><a href="<?=$png_flyers8_url?>"><img src="<?=$png_flyers8_url?>" border="0" alt="Graphic of flyers for printing"></a></p>
 <?
+    }
 }
 
 /* oops CODE

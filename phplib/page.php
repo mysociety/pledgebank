d7302f0ad1504ebfbec4ee55eb80a6b99389619d
matthew <matthew>
2005-04-20 18:04:44 +0000

    UTF-8 meta header (for now).

--- phplib/page.php
+++ phplib/page.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: page.php,v 1.14 2005-04-12 22:58:46 matthew Exp $
+// $Id: page.php,v 1.15 2005-04-20 18:04:44 matthew Exp $
 
 /* page_header TITLE [PARAMS]
  * Print top part of HTML page, with the given TITLE. This prints up to the
@@ -14,7 +14,10 @@
  * then they are not there if the page is printed.  */
 function page_header($title, $params = array()) {
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html lang="en"><head><title><?
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content"text/html; charset=utf-8">
+<title><?
     if ($title) 
         print htmlspecialchars($title) . " - ";
 ?> PledgeBank - Not Finished Yet</title>

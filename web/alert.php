e253d062c7401fe6baff834558606d80f7b26866
francis <francis>
2005-06-11 13:59:46 +0000

    Title same as front page

--- web/alert.php
+++ web/alert.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: francis@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: alert.php,v 1.2 2005-06-09 14:13:10 francis Exp $
+// $Id: alert.php,v 1.3 2005-06-11 13:59:46 francis Exp $
 
 require_once '../phplib/pb.php';
 require_once '../phplib/fns.php';
@@ -65,7 +65,7 @@ function local_alert_subscribe_box() {
 ?>
 <form accept-charset="utf-8" class="pledge" name="pledge" action="/alert" method="post">
 <input type="hidden" name="subscribe_alert" value="1">
-<h2>Pledges in your area!</h2>
+<h2>Get emails about local pledges</h2>
 <p>Fill in the form, and we'll email you when someone creates a new pledge near you.</p>
 <p>
 <label for="email"><strong>Your email:</strong></label> 

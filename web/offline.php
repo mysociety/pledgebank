8be7907b9628db0948ad1460fa72979e6aa73321
chris <chris>
2005-06-12 19:18:18 +0000

    Add picture for "you never bump in to your neighbours online"; typos; note that SMS works only in the UK.

--- web/offline.php
+++ web/offline.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: offline.php,v 1.3 2005-06-12 18:44:12 sandpit Exp $
+// $Id: offline.php,v 1.4 2005-06-12 19:18:18 chris Exp $
 
 require_once "../phplib/pb.php";
 require_once '../phplib/fns.php';
@@ -13,54 +13,64 @@ require_once '../phplib/fns.php';
 page_header("You never bump into your neighbours online");
 
 ?>
-<h2>You never bump into your neighbours online</h2>
+<h2>You never bump in to your neighbours online</h2>
+
+<img src="monitor-neighbours-250px.jpg" title="You never bump in to your neighbours online"
+style="float: left;">
 
 <p>Have you ever noticed that you never bump into your neighbours online?
 This means that most websites can't offer much help to people trying
-to do things on their street or in their local community.
+to do things on their street or in their local community.</p>
 
 <p>PledgeBank is different. We've designed PledgeBank to make it easy to
 get word of your pledges to your neighbours, and easy for them to get
-involved even if they never use the internet.
+involved even if they never use the internet.</p>
 
 <h2>Flyers</h2>
 
 <img src="flyer-example.png" title="Example PledgeBank flyer"
-style="float: right">
+style="float: right;">
 
 <p>Every pledge created on PledgeBank comes with a set of automatically
-generated flyers.
+generated flyers.</p>
 
 <p>These flyers tell people what your pledge is and how to sign up.
 They're ideal for posting through doors, pinning on notice-boards,
 handing out at school gates - whatever you want. If you've created a
 pledge yourself, you can ask your signers to print and distribute
-leaflets themselves, spreading the word further.
+leaflets themselves, spreading the word further.</p>
 
 <p>Each flyer tells people how to sign up either via the web, or via a
-simple two word text message.
+simple two word text message.</p>
 
 <h2>Mobile Phones</h2>
 
-<img src="phone-alone.jpg" style="float:left;
-clear:left; margin-right: 10px">
+<img src="phone-alone.jpg" style="float: left;
+clear:left; margin-right: 10px;">
 
 <p>The ability to sign up via text message is powerful because it means
 that people who never use the Internet can sign up, and you can get
-someone to sign up even if they're nowhere near a computer.
+someone to sign up even if they're nowhere near a computer.</p>
+
+<p>Anyone in the UK with a mobile can sign up to a pledge by texting the word
+<strong>pledge</strong> followed by the pledge reference to
+<strong>60022</strong>.</p>
 
 <p>Text messages cost whatever you normally pay for a normal SMS, and as
 a pledge creator you can write to your mobile signers when your pledge
-succeeds, letting them know what to do next.
+succeeds, letting them know what to do next.</p>
 
 <p>There are all types of other things you can do with text messages. You
 can include the details in your own campaign materials, include them
 in letters to newspapers, pin them in your window, tattoo them on your
-arms.
+arms.</p>
 
 <p>So if you want to create a pledge, or if you have seen one on this
 site that you really endorse, fire up the printer, memorize the text
-message details, and take your message out into the big wide world.
+message details, and take your message out into the big wide world.</p>
+
+<p>(Unfortunately we can't offer the SMS service outside the UK at the
+moment.)</p>
 
 <p><a href="/">To PledgeBank front page</a>
 <?

4e4f5d5cc50eaf8362de07f89117adb165b1919b
francis <francis>
2005-06-18 00:19:10 +0000

    Add sort by when pledge was created

--- web/all.php
+++ web/all.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: francis@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: all.php,v 1.19 2005-06-17 13:44:09 francis Exp $
+// $Id: all.php,v 1.20 2005-06-18 00:19:10 francis Exp $
 
 require_once "../phplib/pb.php";
 require_once '../phplib/fns.php';
@@ -16,7 +16,7 @@ define('PAGE_SIZE', 50);
 
 $err = importparams(
             array('offset', '/^(0|[1-9]\d*)$/', '', 0),
-            array('sort', '/^(title|target|date|name|ref)$/', '', 'date')
+            array('sort', '/^(title|target|date|name|ref|creationtime)$/', '', 'date')
         );
 if ($err) {
     err('Illegal offset or sort parameter passed');
@@ -34,6 +34,10 @@ $ntotal = db_getOne("
 if ($ntotal < $q_offset) 
     $q_offset = $ntotal - PAGE_SIZE;
 
+$sort_phrase = $q_sort;
+if ($q_sort == 'creationtime') {
+    $sort_phrase .= " DESC";
+}
 $qrows = db_query("
         SELECT *, (SELECT count(*) FROM signers
                     WHERE signers.pledge_id = pledges.id) AS signers
@@ -42,7 +46,7 @@ $qrows = db_query("
             AND date >= pb_current_date() 
             AND pin IS NULL 
             AND pb_pledge_prominence(id) <> 'backpage'
-            ORDER BY $q_sort LIMIT ? OFFSET $q_offset", PAGE_SIZE);
+            ORDER BY $sort_phrase LIMIT ? OFFSET $q_offset", PAGE_SIZE);
 /* PG bug: mustn't quote parameter of offset */
 
 print "<h2>All Pledges <small>(which at least a few people have signed up to)</small></h2>";
@@ -64,7 +68,7 @@ if ($ntotal > 0) {
     $navlinks = '<p align="center">' . $prev . ' | Pledges ' . ($q_offset + 1) . ' &ndash; ' . 
         ($q_offset + PAGE_SIZE > $ntotal ? $ntotal : $q_offset + PAGE_SIZE) . ' of ' .
         $ntotal . ' | ' . $next . '<br>Sort by: ';
-    $arr = array('title'=>'Title', 'target'=>'Target', 'date'=>'Deadline', 'name'=>'Creator', 'ref'=>'Short name');
+    $arr = array('creationtime'=>'Created', 'title'=>'Title', 'target'=>'Target', 'date'=>'Deadline', 'name'=>'Creator', 'ref'=>'Short name');
     foreach ($arr as $s => $desc) {
         if ($q_sort != $s) $navlinks .= "<a href=\"all?sort=$s$off\">$desc</a>"; else $navlinks .= $desc;
         if ($s != 'ref') $navlinks .= ' | ';

95c9a7c79f5cf4b3b9c5d8182005b442001b1f6e
matthew <matthew>
2005-08-29 13:29:27 +0000

    Add poEdit instructions.

--- web/translate.php
+++ web/translate.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: translate.php,v 1.3 2005-07-22 11:48:59 matthew Exp $
+// $Id: translate.php,v 1.4 2005-08-29 13:29:27 matthew Exp $
 
 require_once "../phplib/pb.php";
 require_once '../../phplib/utility.php';
@@ -33,6 +33,69 @@ Also, <a href="http://www.mysociety.org/mailman/listinfo/mysociety-i18n">sign up
 that is hard to understand. The instructions assume you're running
 Windows, apologies for that &ndash; users of other operating
 systems should be able to work out what similar steps to take.
+
+<p>There are two ways to translate the file - <a href="#manually">manually</a> or
+<a href="#poedit">using poEdit</a>, a program to help with translations.
+If you're new at translating, I recommend using poEdit, as it
+keeps track of untranslated strings and things like that.
+
+<p>If there are any problems or questions at any stage, <a href="/contact">contact us</a> or ask on the mailing list.
+
+<h3><a name="poedit"></a>Using poEdit</h3>
+
+<ol>
+
+<li><strong>Installing PoEdit:</strong>
+Download poEdit from <a href="http://www.poedit.org/">PoEdit</a>
+(if running Windows, just get the simple installer version)
+and install it.
+
+<li><strong>Getting the original file:</strong>
+Save the following link to somewhere on your computer
+(normally by right-clicking on the link and selecting
+"Save Target As..." or "Save Link As..."):
+<a href="https://secure.mysociety.org/cvstrac/getfile/mysociety/locale/PledgeBank.po">Translation file</a>.
+(If you left click on the link, you can File-&gt;Save As... and then click Back to get back here.)
+
+<li><strong>Opening the file:</strong>
+Open poEdit (Start-&gt;Programs-&gt;poEdit-&gt;poEdit). If this is the first time you've run it, it will ask for your name and email address, to put them in the translation file. Then go to File-&gt;Open and open up the file you have just downloaded.
+<em>Don't be worried by the number of strings to translate!</em>
+You do not have to translate every line before sending
+us what you have done &ndash; the site will simply default
+to English for anything yet to be translated.
+
+<li><strong>Basic translation:</strong>
+Select a line to translate in the top frame, and enter the translation in the bottom. poEdit keeps track of which lines have not yet been translated, and which are "fuzzy" translations (automated translations that may be wrong and should be checked).
+poEdit will also tell you if a sentence has a plural (ie. needs a different translation depending on the number of things being printed out). This should be straightforward.
+File-&gt;Save or the Save button will save your translations to the same file.
+
+<li><p><strong>% placeholders:</strong>
+Sadly, these are a bit
+more complicated. The first are %&nbsp;placeholders.
+These appear within text and stand for something missing &ndash; %d
+means a number, %s means some more text. For example, a sentence to
+give the colour of a cat and its size, in Spanish:
+
+<pre>"The cat is %s and %d centimetres long."
+"El gato tiene %s y %d cent&iacute;metros de largo."</pre>
+
+<p>The colours would be translated separately elsewhere in the file.
+If a situation arises where you need to refer to the % placeholders
+in a different order from the English, you need to specify which
+placeholder goes where. Here's an example, but ask if this comes up
+in your translation (hopefully it won't :) ) :
+
+<pre>"I have %s the %s."                (I have <em style="color: #ff0000">read</em> the <em style="color: #00ff00">book</em>.)
+"Ich habe das %2\$s %1\$s."        (Ich habe das <em style="color: #00ff00">Buch</em> <em style="color: #ff0000">gelesen</em>.)
+</pre>
+
+<li>Email me with as much or as little as you have done, and I will get it up and running as soon as possible. :-)
+
+<li>Oh, one other thing that needs translating are the emails we send out. <a href="https://secure.mysociety.org/cvstrac/dir?d=mysociety/pb/templates/emails">These are available</a>, and are free-standing translations. Ask for more information about this.
+
+</ol>
+
+<h3><a name="manually"></a>Manually</h3>
 
 <ol>
 
@@ -120,7 +183,6 @@ msgstr[1] "Eu tenho %d c&atilde;es."</pre>
 
 <li>Oh, one other thing that needs translating are the emails we send out. <a href="https://secure.mysociety.org/cvstrac/dir?d=mysociety/pb/templates/emails">These are available</a>, and are free-standing translations. Ask for more information about this.
 
-<li>Any problems or questions, <a href="/contact">contact us</a> or ask on the mailing list.
 </ol>
 
 <p align="right">Matthew Somerville<br>30<sup>th</sup> June 2005</p>

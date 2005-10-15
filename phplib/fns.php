10f460b1cd6b5205783cefc2d3dc7026f563fd72
francis <francis>
2005-10-15 00:36:47 +0000

    Cope with fewer than 2 languages

--- phplib/fns.php
+++ phplib/fns.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: fns.php,v 1.78 2005-10-14 12:43:32 francis Exp $
+// $Id: fns.php,v 1.79 2005-10-15 00:36:47 francis Exp $
 
 require_once '../phplib/alert.php';
 require_once '../phplib/microsites.php';
@@ -477,7 +477,10 @@ function pb_print_change_language_links($path = null) {
     }
     $first = array_splice($out, 0, -2);
     if (count($first)) print ' ' . join(', ', $first) . ',';
-    print ' ' . $out[count($out)-2] . ' and ' . $out[count($out)-1];
+    if (count($out) >= 2)
+        print ' ' . $out[count($out)-2] . ' and ' . $out[count($out)-1];
+    elseif (count($out) == 1)
+        print ' ' . $out[0];
     print '. <br><a href="/translate/">'._('Translate PledgeBank into your language').'</a>.';
 }
 

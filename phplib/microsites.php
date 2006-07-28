b5caa0a8967465a35d97f90e15edb2bd943a2b1d
francis <francis>
2006-07-28 00:27:21 +0000

    Oops

--- phplib/microsites.php
+++ phplib/microsites.php
@@ -18,7 +18,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * Email: francis@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: microsites.php,v 1.31 2006-07-28 00:25:39 francis Exp $
+ * $Id: microsites.php,v 1.32 2006-07-28 00:27:21 francis Exp $
  * 
  */
 
@@ -476,7 +476,6 @@ function microsites_redirect_external_login() {
             $url .= "&stashpost=$st";
         else
             $url .= "?stashpost=$st";
-            print $url;exit;
         header("Location: http://www.global-cool.com:8080/auth/?next=" . urlencode($url));
         exit;
     }

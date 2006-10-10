9e34e7d2a83e6d415610499fcc98e87dad9c4ebe
matthew <matthew>
2006-10-10 14:26:36 +0000

    Lucky no-one obeys standards.

--- web/pics.php
+++ web/pics.php
@@ -6,7 +6,7 @@
  * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
  * Email: chris@mysociety.org; WWW: http://www.mysociety.org/
  *
- * $Id: pics.php,v 1.7 2006-06-02 09:44:18 chris Exp $
+ * $Id: pics.php,v 1.8 2006-10-10 14:26:36 matthew Exp $
  * 
  */
 
@@ -32,9 +32,9 @@ $data = pg_unescape_bytea($data);
 if (preg_match('/.jpe?g$/i', $file))
     header("Content-type: image/jpeg");
 elseif (preg_match('/.gif/i', $file))
-    header("Content-type: image/jpeg");
+    header("Content-type: image/gif");
 elseif (preg_match('/.png/i', $file))
-    header("Content-type: image/jpeg");
+    header("Content-type: image/png");
 else
     err('Unknown image type');
 

728237dd3e2e70a6b0d88a88275a14b096aef47d
francis <francis>
2007-05-20 05:09:21 +0000

    Update Javascript text strings for Chinese

---
+++ web/js/pb.be.js
@@ -0,0 +1,25 @@
+/*
+ * pb.be.js
+ * Translation of JS strings
+ * Auto-generated from .po files by gettext-makejs
+ *
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: matthew AT mysociety.org. WWW: http://www.mysociety.org/
+ *
+ */
+
+var translation = {
+"Town:":"Горад/мястэчка:",
+"(choose one)":"(абярыце штосьці)",
+"other local people":"асобам",
+"<Enter your name>":"<Увядзіце Вашае імя>",
+"<Enter your pledge>":"<Увядзіце абяцаньне>",
+"<Enter town or keyword>":"горад або ключавае слова>",
+"<Date>":"<Дата>",
+"other local person":"другі месьціч",
+"Global":"Глябальна",
+"(separator)":"(знак разьдзелу)",
+"Postcode or town:":"Індэкс або места:",
+"You have used %d characters; %d remain.":"Скарыстана %d сымбаляў; засталося %d.",
+"You have used %d characters, which is %d more than will fit in an SMS. Please make your message shorter.":"Паведамленьне ўтрымлівае %d сымбаляў, што на %d болей, чым зьмяшчае адна смска. Калі ласка, скараціце паведамленьне."
+}
---
+++ web/js/pb.pl.js
@@ -0,0 +1,25 @@
+/*
+ * pb.pl.js
+ * Translation of JS strings
+ * Auto-generated from .po files by gettext-makejs
+ *
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: matthew AT mysociety.org. WWW: http://www.mysociety.org/
+ *
+ */
+
+var translation = {
+"Town:":"",
+"(choose one)":"",
+"other local people":"",
+"<Enter your name>":"",
+"<Enter your pledge>":"Wpisz swoją obietnicę",
+"<Enter town or keyword>":"",
+"<Date>":"",
+"other local person":"",
+"Global":"",
+"(separator)":"",
+"Postcode or town:":"",
+"You have used %d characters; %d remain.":"",
+"You have used %d characters, which is %d more than will fit in an SMS. Please make your message shorter.":""
+}

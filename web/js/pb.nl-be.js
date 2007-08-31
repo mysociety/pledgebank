1bcd072ac120f7aac71a582dd38df4cdff251588
matthew <matthew>
2007-08-31 08:24:27 +0000

    Update nl_BE too, and add its JS file and ignore its .mo file.

---
+++ web/js/pb.nl-be.js
@@ -0,0 +1,25 @@
+/*
+ * pb.nl-be.js
+ * Translation of JS strings
+ * Auto-generated from .po files by gettext-makejs
+ *
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: matthew AT mysociety.org. WWW: http://www.mysociety.org/
+ *
+ */
+
+var translation = {
+"Town:":"Stad:",
+"(choose one)":"(kies er één)",
+"other local people":"andere plaatselijke personen",
+"<Enter your name>":"<Typ uw naam in>",
+"<Enter your pledge>":"<Typ uw belofte in>",
+"<Enter town or keyword>":"<Typ stad of sleutelwoord in>",
+"<Date>":"<Datum>",
+"other local person":"andere plaatselijke persoon",
+"Global":"Wereldwijd",
+"(separator)":"(scheidingsteken)",
+"Postcode or town:":"Postcode of stad:",
+"You have used %d characters; %d remain.":"U heeft %d tekens gebruikt; blijven %d.",
+"You have used %d characters, which is %d more than will fit in an SMS. Please make your message shorter.":"U heeft %d tekens gebuikt en dat is %d meer dan het maximum voor een SMS. Gelieve uw bericht in te korten."
+}

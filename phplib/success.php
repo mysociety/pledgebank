02faf665f113a96339d61c87c9fa0ed832579ac1
matthew <matthew>
2007-10-12 16:39:20 +0000

    Make images links too.

--- phplib/success.php
+++ phplib/success.php
@@ -5,13 +5,13 @@
 // Copyright (c) 2007 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: success.php,v 1.3 2007-10-12 15:01:23 matthew Exp $
+// $Id: success.php,v 1.4 2007-10-12 16:39:20 matthew Exp $
 
 $success_stories = array(
 array('summary' => '
 <h3><a href="/success/airfarespledge">' . _('Organise aid trip to New Orleans') . ' &ndash; airfarespledge</a></h3>
-<p><img src="/i/airfarespledge.tn.jpeg" class="r" title="' . _('My best friend in Biloxi MS') . '"
-alt="' . _('The creator of the pledge with a friendly dog, tagged 595') . '">' .
+<p><a href="/success/airfarespledge"><img src="/i/airfarespledge.tn.jpeg" class="r" title="' . _('My best friend in Biloxi MS') . '"
+alt="' . _('The creator of the pledge with a friendly dog, tagged 595') . '"></a>' .
 p(_('&ldquo;After seeing the news about Hurricane Katrina, which had some
 pretty graphic pictures showing the damage, I thought of how I could
 help....Being a fan of Jazz music and knowing a lot about New Orleans,
@@ -21,8 +21,8 @@ donating their time and money, and being an international person, donating
 money, just didn&rsquo;t work for me.')) .
 p('<a href="/success/airfarespledge">' . _('Continue reading&hellip;') . '</a>'),
 'entry' => '
-<p><img class="r" src="/i/airfarespledge.tn.jpeg" title="' . _('My best friend in Biloxi MS') . '"
-alt="' . _('The creator of the pledge with a friendly dog, tagged 595') . '">
+<p><a href="/success/airfarespledge"><img class="r" src="/i/airfarespledge.tn.jpeg" title="' . _('My best friend in Biloxi MS') . '"
+alt="' . _('The creator of the pledge with a friendly dog, tagged 595') . '"></a>
 <h3><a href="/success/airfarespledge">' . _('Organise aid trip to New Orleans') . ' &ndash; airfarespledge</a></h3>' .
 p(_('&ldquo;After seeing the news about Hurricane Katrina, which had some
 pretty graphic pictures showing the damage, I thought of how I could
@@ -38,8 +38,8 @@ p('<a href="/success/airfarespledge">' . _('Continue reading&hellip;') . '</a>')
 ), array(
 'summary' => '
 <h3><a href="/electionwatch07">' . _('Gather people to monitor elections') . ' &ndash; electionwatch07</a></h3>
-<p><img class="r" alt="' . _('Election observer Becky, outside a polling station') .'"
-src="http://www.openrightsgroup.org/wp-content/uploads/observers.jpg" width="195" height="200"></p>' .
+<p><a href="/electionwatch07"><img class="r" alt="' . _('Election observer Becky, outside a polling station') .'"
+src="http://www.openrightsgroup.org/wp-content/uploads/observers.jpg" width="195" height="200"></a></p>' .
 p(_('&ldquo;Basically, without PledgeBank I doubt we would have had nearly
 as many election observers volunteer. The public nature of PledgeBank also
 seemed to encourage the very high follow-through rate which meant that most
@@ -50,8 +50,8 @@ to observe elections in Scotland and England. Their observations fed into
 title="ORG&rsquo;s report into the May 2007 elections">our 65-page
 report</a> which was recently launched with significant press
 coverage.&rdquo;')), 'entry' => '
-<p><img class="l" alt="' . _('Election observer Becky, outside a polling station') . '"
-src="http://www.openrightsgroup.org/wp-content/uploads/observers.jpg" width="195" height="200"></p>
+<p><a href="/electionwatch07"><img class="l" alt="' . _('Election observer Becky, outside a polling station') . '"
+src="http://www.openrightsgroup.org/wp-content/uploads/observers.jpg" width="195" height="200"></a></p>
 <h3><a href="/electionwatch07">' . _('Gather people to monitor elections') . ' &ndash; electionwatch07</a></h3>' .
 p(_('&ldquo;Basically, without PledgeBank I doubt we would have had nearly
 as many election observers volunteer. The public nature of PledgeBank also
@@ -66,15 +66,15 @@ coverage.&rdquo;')),
 ), array(
 'summary' => '
 <h3><a href="/First1000">' . _('Get 1000 people to move house') .' &ndash; First1000</a></h3>
-<p><img class="r" alt="' . _('First1000 logo') . '"
-src="http://www.pledgebank.com/pics/First1000.gif" width="100" height="90"></p>
+<p><a href="/First1000"><img class="r" alt="' . _('First1000 logo') . '"
+src="http://www.pledgebank.com/pics/First1000.gif" width="100" height="90"></a></p>
 ' . p(_('&ldquo;The membership of the
 Free State Project was very active in recruiting signers, and as a result
 of the pledge, 1000 people will be moving to New Hampshire by the end of
 2008 in order to become activists for liberty!&rdquo;')),
 'entry' => '
-<p><img class="r" alt="' . _('First1000 logo') . '"
-src="http://www.pledgebank.com/pics/First1000.gif" width="100" height="90"></p>
+<p><a href="/First1000"><img class="r" alt="' . _('First1000 logo') . '"
+src="http://www.pledgebank.com/pics/First1000.gif" width="100" height="90"></a></p>
 <h3><a href="/First1000">' . _('Get 1000 people to move house') .' &ndash; First1000</a></h3>
 ' . p(_('&ldquo;The membership of the
 Free State Project was very active in recruiting signers, and as a result
@@ -83,8 +83,8 @@ of the pledge, 1000 people will be moving to New Hampshire by the end of
 ), array(
 'summary' => '
 <h3><a href="http://promise.livesimply.org.uk/">' . _('Changing lifestyles') . ' &ndash; LiveSimply</a></h3>' .
-p('<img class="r" alt="' . _('LiveSimply logo') . '"
-src="http://promise.livesimply.org.uk/microsites/livesimply/promise_banner_right.jpg">') .
+p('<a href="http://promise.livesimply.org.uk/"><img class="r" alt="' . _('LiveSimply logo') . '"
+src="http://promise.livesimply.org.uk/microsites/livesimply/promise_banner_right.jpg"></a>') .
 p(_('&ldquo;&hellip;having a website where you can get that colourful data
 of people&rsquo;s thoughts, experience and opinions can provide us with
 some insight in how people are changing their lives.')) .
@@ -100,8 +100,8 @@ their lifestyle, which is a qualitative objective (i.e it&rsquo;s hard to
 measure it with numbers). So having a website where you can get that
 colourful data of people&rsquo;s thoughts, experience and opinions can
 provide us with some insight in how people are changing their lives.')) .
-p('<img class="r" alt="' . _('LiveSimply logo') . '"
-src="http://promise.livesimply.org.uk/microsites/livesimply/promise_banner_right.jpg">'
+p('<a href="http://promise.livesimply.org.uk/"><img class="r" alt="' . _('LiveSimply logo') . '"
+src="http://promise.livesimply.org.uk/microsites/livesimply/promise_banner_right.jpg"></a>'
 . _('&ldquo;The features PledgeBank has were suitable for our needs - we
 wanted to create a community of people who make promises so that they can
 support each other. We could just go for a simpler system where people
@@ -117,8 +117,8 @@ I like the ethical background of the organisation.&rdquo;')),
 ), array(
 'summary' => '
 <h3><a href="/SaveTheSycamore">' . _('Campaign to save a local tree') . ' &ndash; SaveTheSycamore</a></h3>
-<p><img class="r" alt="' . _('Save The Sycamore') . '" width="150" height="200"
-src="http://www.pledgebank.com/pics/SaveTheSycamore.jpeg"></p>' .
+<p><a href="/SaveTheSycamore"><img class="r" alt="' . _('Save The Sycamore') . '" width="150" height="200"
+src="http://www.pledgebank.com/pics/SaveTheSycamore.jpeg"></a></p>' .
 p(_('&ldquo;The amount of elderly people who supported me in this pledge by
 writing and phoning the council was phenomenal. Thank you so much for
 affording us a professional launch pad. My next pledge will be to promote
@@ -127,8 +127,8 @@ opportunity to make a difference. I truly am
 <strong>astounded</strong>!!!!!! With thanks from myself, family and my
 community.&rdquo;')),
 'entry' => '
-<p><img class="l" alt="' . _('Save The Sycamore') . '" width="150" height="200"
-src="http://www.pledgebank.com/pics/SaveTheSycamore.jpeg"></p>
+<p><a href="/SaveTheSycamore"><img class="l" alt="' . _('Save The Sycamore') . '" width="150" height="200"
+src="http://www.pledgebank.com/pics/SaveTheSycamore.jpeg"></a></p>
 <h3><a href="/SaveTheSycamore">' . _('Campaign to save a local tree') . ' &ndash; SaveTheSycamore</a></h3>' .
 p(_('&ldquo;The amount of elderly people who supported me in this pledge by
 writing and phoning the council was phenomenal. Thank you so much for

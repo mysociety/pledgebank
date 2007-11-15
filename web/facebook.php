18e174563f487a8ba9fefc12436655bad31c01c6
francis <francis>
2007-11-15 11:21:00 +0000

    Reorganise todo a bit.

--- web/facebook.php
+++ web/facebook.php
@@ -6,72 +6,68 @@
 // Copyright (c) 2007 UK Citizens Online Democracy. All rights reserved.
 // Email: francis@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: facebook.php,v 1.61 2007-11-15 10:41:53 francis Exp $
+// $Id: facebook.php,v 1.62 2007-11-15 11:21:00 francis Exp $
 
 /*
-
-
 TODO:
 
-- Check out notifications.sendEmail, which doesn't seem documented yet
-    http://wiki.developers.facebook.com/index.php/Push_Changes_-_Nov_13_2007
 - Matthew doesn't get notifications :(
+- Check out notifications.sendEmail
+    http://developers.facebook.com/documentation.php?v=1.0&method=notifications.sendEmail
+- Can send plain text as well as FBML version now?
 
 - 64 bit ids http://developers.facebook.com/news.php?blog=1&story=45
 
-- Add link to source code
+- On pledge listing pages play with showing:
+    * pledges which have lots of Facebook signers
+    * featured pledges in facebook via mutual signer connections (first need to fix connections to use Facebook ids as well as emails?)
+    * pledges made by / signed by people in same network (e.g. university) as you
+    * fastest growing pledges
+    * pledges signed by people with similar interests to you
+- Show in "friends pledges" section ones *created* by friends separately
 
 - Show explicitly on e.g. http://apps.facebook.com/pledgebank/list/friends
   that a pledge is closed.
-- And also that you have already signed it. Say something about, have
-   you don it yet.
-- "you has created these pledges" on profile
+- And also if you have already signed it.
+- The link to the pledge being the title part doesn't make it clear you can
+  click the link, especially when the sign button is there. Not sure what to
+  do. Maybe make title 'Pledge "buttonmooon"' the link?
 
-- Text on message for "share" links is not perfect.
-- Text on minifeed is not perfect
+- "Share" links just uses text "Sign this pledge", could be better
+- Text used in feed story should be better (maybe use pledge ref as in
+  notifications short text)
 
-- Let creators of pledges use fancy form to share it.
 
-- Announce message shouldn't link to pledgebank.com, but to facebook.com
+- Ability to link existing PledgeBank account with Facebook account, so can show pledges already 
+  created/signed on profile. (Put link on profile message "has not signed any pledges in Facebook"
+  amongst other places)
+- Ability to add pledges you haven't signed to your profile.
+- Ability to remove pledges that you created/signed from your profile.
+- Option to have dynamically changing featured pledges appear on your profile.
 
-- Extra login dialog which you get if you try to sign a pledge you haven't seen before
-
-- Richard (and others) want to be able to add app and link to existing PB account so can show pledges on his profile
-  And to be able to add it to show his willingness to use it, but without signing a pledge yet
-  link from profile message "has not signed any pledges in Facebook"
+- Let people say "I've done it!" on the pledges.
+- Send questionnaire to ask if they've done it.
+- On profile page, highlight more successful pledges, and that you must do them / have done them.
 
 - Use friend request form (mini?) rather than share link everywhere, as has confused people
   by starting a large thread. Maybe have "invite your friends" link explicitly?
+- Let creators of pledges use fancy form to share it.
 
-Improvements:
-- Show pledges which have lots of Facebook signers
+- Announce message shouldn't link to pledgebank.com, but to facebook.com
+- Somehow remove extra login dialog which you get if you try to sign a pledge
+  you haven't seen before, that is caused by post button. Not sure can be done
+  really.
+
 - Link to creators of via facebook pledges, and show their photo
-- Show in "friends pledges" section ones created by friends separately
-- Show on http://apps.facebook.com/pledgebank ones you've already signed clearer
-- On profile page, highlight more successful pledges, and that you must do them.
-- Let people say "I've done it!" on the pledges on their profile.
-- Let people remove pledges from their profile page
-- Include featured pledges on EVERYONE's profile optionally
-
-- Show featured pledges in facebook via mutual signer connections
-  (first need to fix connections to use Facebook ids as well as emails?)
-- Show pledges made by / signed by people in same network as you
-- Show fastest growing pledges
-- Find pledges by people with similar interests
+- Tidy up display when lots of signers http://apps.facebook.com/pledgebank/Pauls-100mill
 
 - Call http://wiki.developers.facebook.com/index.php/Feed.publishTemplatizedAction
   instead of PublishActionofUser
   See http://www.facebook.com/topic.php?uid=2205007948&topic=13926&start=30&hash=503b3e81edba5aeff0c1ac98eb58a61a at end for PHP function to add to the API file
-- Tidy up display when lots of signers http://apps.facebook.com/pledgebank/Pauls-100mill
-
-Later:
-- Cache names of Facebook signers in the person table
-- Add chivvy email about Facebook
-- Aggregate multiple announce messages more intelligently - if user posts two messages
-  within 12 hours, then only post once on news feed. Or link to specific messages from
-  news feed, rather than to general pledge.
 
 Not so important:
+- Cache names of Facebook signers in the person table
+- Add chivvy email about Facebook
 - Test what happens if you add app, but refuse each of the major permissions
 - Lower case and fuzzy matching of pledge refs
 - Detect language that Facebook is using, and tell PledgeBank pages to use that.

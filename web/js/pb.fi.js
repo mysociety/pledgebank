ad922e636e0a929ab898d514e1ac8cd22f511035
matthew <matthew>
2008-03-04 17:35:58 +0000

    Update of others too.

---
+++ web/js/pb.fi.js
@@ -0,0 +1,28 @@
+/*
+ * pb.fi.js
+ * Translation of JS strings
+ * Auto-generated from .po files by gettext-makejs
+ *
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: matthew AT mysociety.org. WWW: http://www.mysociety.org/
+ *
+ */
+
+var translation = {
+"Town:":"Kaupunki:",
+"(choose one)":"(valitse yksi)",
+"Place name:":"Paikan nimi:",
+"other local people":"muita paikallisia ihmisiä",
+"<Enter your name>":"<Kirjoita nimesi>",
+"<Enter your pledge>":"<Anna lupauksesi>",
+"<Enter town or keyword>":"<Anna kaupunki tai avainsana>",
+"<Date>":"<Päivämäärä>",
+"other local person":"muu paikallinen ihminen",
+"Global":"Maailmanlaajuinen",
+"(separator)":"(erottaja)",
+"Postcode or town:":"Postinumero tai kaupunki:",
+"Town or zipcode:":"Kaupunki tai postinumero:",
+"Place name or zipcode:":"Paikan nimi tai postinumero:",
+"You have used %d characters; %d remain.":"Olet käyttänyt %d merkkiä; %d jäljellä.",
+"You have used %d characters, which is %d more than will fit in an SMS. Please make your message shorter.":"Olet käyttänyt %d merkkiä, mikä on %d enemmän kuin tekstariin mahtuu. Lyhennä viestiäsi."
+}
---
+++ web/js/pb.ja.js
@@ -0,0 +1,28 @@
+/*
+ * pb.ja.js
+ * Translation of JS strings
+ * Auto-generated from .po files by gettext-makejs
+ *
+ * Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
+ * Email: matthew AT mysociety.org. WWW: http://www.mysociety.org/
+ *
+ */
+
+var translation = {
+"Town:":"",
+"(choose one)":"",
+"Place name:":"",
+"other local people":"",
+"<Enter your name>":"",
+"<Enter your pledge>":"",
+"<Enter town or keyword>":"",
+"<Date>":"",
+"other local person":"",
+"Global":"",
+"(separator)":"",
+"Postcode or town:":"",
+"Town or zipcode:":"",
+"Place name or zipcode:":"",
+"You have used %d characters; %d remain.":"",
+"You have used %d characters, which is %d more than will fit in an SMS. Please make your message shorter.":""
+}

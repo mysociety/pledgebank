34e0730fd849ff710e94cd330fa140e4aa7151b6
matthew <matthew>
2008-04-03 08:01:28 +0000

    Chinese banner image.

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: index.php,v 1.261 2008-02-12 10:22:17 matthew Exp $
+// $Id: index.php,v 1.262 2008-04-03 08:01:28 matthew Exp $
 
 // Load configuration file
 require_once "../phplib/pb.php";
@@ -16,6 +16,8 @@ require_once '../phplib/pbperson.php';
 require_once '../phplib/success.php';
 require_once '../../phplib/utility.php';
 
+$banner_src = 'howitworks.png';
+if ($lang == 'zh') $banner_src = 'howitworks_zh.png';
 page_header(null, 
     array(
         'rss'=> array(
@@ -25,7 +27,7 @@ page_header(null,
         ), 
         'id' => 'front',
         'cache-max-age' => 600,
-        'banner' => $microsite ? '' : '<p id="banner"><img src="/i/howitworks.png" alt="' . _('How PledgeBank works: PledgeBank is a free site to help people get things done - especially things that require several people. It is very simple - you make a pledge, set a target, find people to agree and sign the pledge, and succeed!') . '"></p>',
+        'banner' => $microsite ? '' : '<p id="banner"><img src="/i/' . $banner_src . '" alt="' . _('How PledgeBank works: PledgeBank is a free site to help people get things done - especially things that require several people. It is very simple - you make a pledge, set a target, find people to agree and sign the pledge, and succeed!') . '"></p>',
     )
 );
 debug_comment_timestamp("after page_header()");

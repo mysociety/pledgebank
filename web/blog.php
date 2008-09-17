16b0d93251c9953bda5f5d03196618f2f3a50355
matthew <matthew>
2008-09-17 23:01:47 +0000

    Fix.

--- web/blog.php
+++ web/blog.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2008 UK Citizens Online Democracy. All rights reserved.
 // Email: matthew@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: blog.php,v 1.2 2008-08-25 18:53:21 matthew Exp $
+// $Id: blog.php,v 1.3 2008-09-17 23:01:47 matthew Exp $
 
 require_once "../phplib/pb.php";
 
@@ -16,7 +16,7 @@ if (!$ref)
     err('No blog reference given', E_USER_NOTICE);
 
 $f = file_get_contents('http://www.mysociety.org/' . $ref);
-preg_match('#<h2>(.*?)</h2>\s*<div class="entry">\s*(.*?)<p class="postmetadata[^>]*>\s*<small>\s*(.*?)and is filed#s', $f, $m);
+preg_match('#<h1>(.*?)</h1>.*?<div class="entry"[^>]*>(.*?)<p class="postmetadata[^>]*>\s*<small>\s*(.*?)and is filed#s', $f, $m);
 $title = $m[1];
 $content = $m[2];
 $meta = $m[3];

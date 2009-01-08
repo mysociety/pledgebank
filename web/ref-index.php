33769345f90475490c9f1a02f5dc18e2688e12cf
angie <angie>
2009-01-08 15:37:14 +0000

    undo button

--- web/ref-index.php
+++ web/ref-index.php
@@ -5,7 +5,7 @@
 // Copyright (c) 2005 UK Citizens Online Democracy. All rights reserved.
 // Email: francis@mysociety.org. WWW: http://www.mysociety.org
 //
-// $Id: ref-index.php,v 1.131 2009-01-08 15:21:30 angie Exp $
+// $Id: ref-index.php,v 1.132 2009-01-08 15:37:14 angie Exp $
 
 require_once '../conf/general';
 require_once '../phplib/page.php';
@@ -247,13 +247,16 @@ function draw_signatories($p) {
                 print '<li id="signer' . $r['id'] . '"';
                 if ($r['done']=='t') print ' class="done"';
                 print '>';
-                if (microsites_has_survey() && $r['done']=='f' && !is_null($P) && $r['person_id'] == $P->id())
+                if (microsites_has_survey() && $r['done']=='f' && !is_null($P) && $r['person_id'] == $P->id()) {
                     print '<form method="post" action="' . $p->url_survey() . '"><input type="hidden" name="r" value="pledge">';
-                print htmlspecialchars($r['name']);
-                if (microsites_has_survey() && $r['done']=='f' && !is_null($P) && $r['person_id'] == $P->id())
+                    print htmlspecialchars($r['name']);
                     print ' &ndash; <input type="submit" value="'._("I have now done what I pledged").'"></form>';
-                if (microsites_has_survey() && $r['done']=='t' && !is_null($P) && $r['person_id'] == $P->id())
-                    print ' &ndash; <form method="post" action="' . $p->url_survey() . '"><input type="hidden" name="r" value="pledge"><input type="hidden" name="undopledge" value="1"><input type="submit" value="'._("I have NOT done what I pledged").'"></form>';
+                }
+                if (microsites_has_survey() && $r['done']=='t' && !is_null($P) && $r['person_id'] == $P->id()) {
+                    print '<form method="post" action="' . $p->url_survey() . '"><input type="hidden" name="r" value="pledge">'
+                    print htmlspecialchars($r['name']);
+                    print ' &ndash; <input type="hidden" name="undopledge" value="1"><input type="submit" value="'._("I have NOT done what I pledged").'"></form>';
+                }
                 print '</li>';
             } else {
                 err('showname set but no name');

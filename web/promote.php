eac44fe1350c6fdb2a36b4f400085884f5576bdc
matthew <matthew>
2010-01-19 16:26:45 +0000

    Update promote links/images.

--- web/promote.php
+++ web/promote.php
@@ -17,22 +17,22 @@ print p(_('For a library or similar'));
 
 ?>
 
-<p><a href="http://memedev.com/mysocietydownloads/promo/pbposter.pdf"><img src="http://memedev.com/mysocietydownloads/promo/pbposter.png" alt="poster thumbnail"></a></p>
+<p><a href="/promo/pbposter.pdf"><img src="/promo/pbposter.png" alt="poster thumbnail"></a></p>
 </div>
 
 <?
 
 print h3(_('Buttons for your blog'));
-display('<a href="http://www.pledgebank.com/"><img src="http://memedev.com/mysocietydownloads/promo/pb1.png"/></a>');
-display('<a href="http://www.pledgebank.com/"><img src="http://memedev.com/mysocietydownloads/promo/pb2.png"/></a>');
+display('<a href="http://www.pledgebank.com/"><img src="http://www.pledgebank.com/promo/pb1.png"/></a>');
+display('<a href="http://www.pledgebank.com/"><img src="http://www.pledgebank.com/promo/pb2.png"/></a>');
 print h3(_('Bigger banner'));
-display('<a href="http://www.pledgebank.com/"><img src="http://memedev.com/mysocietydownloads/promo/pb_large.png"/></a>');
+display('<a href="http://www.pledgebank.com/"><img src="http://www.pledgebank.com/promo/pb_large.png"/></a>');
 print h3(_('Pledgebank plugin for Wordpress'));
 
 ?>
 
-<p><img src="http://memedev.com/mysocietydownloads/promo/pbwordpress.png" alt="pledgebank plugin for wordpress screenshot"></p>
-<small><a href="http://memedev.com/mysocietydownloads/promo/wordpresspledgebank.com.zip"><?=_('Get the code') ?></a></small> 
+<p><img src="/promo/pbwordpress.png" alt="pledgebank plugin for wordpress screenshot"></p>
+<small><a href="/promo/wordpresspledgebank.com.zip"><?=_('Get the code') ?></a></small> 
 
 <?
 

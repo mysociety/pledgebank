c3e6674150e7e40d88e87705f4ddc660aa85e18f
Louise Crow <louise.crow@gmail.com>
2010-05-17 16:24:54 +0100

    New piwik tracker

--- phplib/page.php
+++ phplib/page.php
@@ -341,18 +341,19 @@ function page_footer($params = array()) {
             // Hopefully permanently use piwik :)
             if (true) {
 ?>
+
 <!-- Piwik -->
-<script src="http://piwik.mysociety.org/piwik.js" type="text/javascript"></script>
 <script type="text/javascript">
-<!--
-piwik_action_name = '';
-piwik_idsite = 1;
-piwik_url = 'http://piwik.mysociety.org/piwik.php';
-piwik_log(piwik_action_name, piwik_idsite, piwik_url);
-//-->
-</script>
-<noscript><img src="http://piwik.mysociety.org/piwik.php?idsite=1" style="border:0" alt=""></noscript>
-<!-- /Piwik --> 
+var pkBaseURL = (("https:" == document.location.protocol) ? "https://piwik.mysociety.org/" : "http://piwik.mysociety.org/");
+document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
+</script><script type="text/javascript">
+try {
+var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+piwikTracker.trackPageView();
+piwikTracker.enableLinkTracking();
+} catch( err ) {}
+</script><noscript><p><img src="http://piwik.mysociety.org/piwik.php?idsite=1" style="border:0" alt=""/></p></noscript>
+<!-- End Piwik Tag --> 
 <script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
 </script>
 <script type="text/javascript">

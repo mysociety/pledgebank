5b51e3d7a84900037da4728b43c84900926947ee
Matthew Somerville <matthew@mysociety.org>
2010-11-11 16:34:56 +0000

    Remove 365act stuff.

--- phplib/microsites.php
+++ phplib/microsites.php
@@ -28,7 +28,6 @@
 /* Codes of microsites, and name displayed next to PledgeBank logo */
 $microsites_list = array('everywhere' => _('Everywhere'),
                          'london' => 'London',
-                         '365act' => '365 Ways',
                          'glastonbury' => 'Glastonbury',
                          'interface' => 'Interface',
                          #'global-cool' => 'Global Cool', # no longer used, can remove all its code at some point
@@ -209,24 +208,6 @@ function microsites_logo() {
 <span id="countrytitle"><img src="/microsites/interface-logo.gif" alt="interface">
 <a href="/where">' . _('(change)') . '</a></span>
 <span id="tagline"><small><br>' . _('I&rsquo;ll do it, but <strong>only</strong> if you&rsquo;ll help') . '</small></span></h1>';
-    } elseif ($microsite && $microsite == '365act') {
-        return '
-<a href="http://www.365act.com"><img src="/microsites/365-logo-small.png" alt="365 Act" align="left"
-    style="
-    margin-top: -10px; 
-    margin-left: -0.4em 
-    background-color: #ffffff;
-    float: left;
-    border: solid 2px #21004a;
-    padding: 0px;
-    margin: 10px;
-    "></a>
-<h1>
-<a id="logo" href="/"><span id="logo_pledge">Pledge</span><span id="logo_bank">Bank</span></a><span id="beta">Beta</span>
-<span id="countrytitle"><a href="/where">' . _('(other PledgeBanks)') . '</a></span>
-<span id="tagline"><small><br>' . _('I&rsquo;ll do it, but <strong>only</strong> if you&rsquo;ll help') . '</small></span></h1>
-';
-
     } elseif ($microsite && $microsite == 'london') {
         return '
 <h1><a id="logo" href="/"><span id="logo_pledge">Pledge</span><span id="logo_bank">Bank</span> <span id="logo_pledge">London</span></a>
@@ -298,7 +279,6 @@ function microsites_css_files() {
     if ($microsite && in_array($microsite, array(
                 'interface', 
                 'glastonbury', 
-                '365act', 
                 'london', 
                 'global-cool',
                 'catcomm',
@@ -399,21 +379,6 @@ function microsites_frontpage_intro() {
 internal use at Interface. PledgeBank is a handy tool which is good at
 getting people to do social or environmental things they
 want to do but normally never get round to.</p><?
-    } elseif ($microsite == '365act') {
-        ?><h2>Pledge to change the world today!</h2>
-
-        <p>If you agree to do something to change the world... and if lots of
-        others also agree to do the same thing... then this will create a much
-        bigger change.</p>
-
-        <p>This is the spirit of “365 Ways to Change the World” &mdash; lots of people
-        all over the world doing lots of things to make a better world.</p>
-
-        <p>So, make a pledge now to change the world. And ask all your friends and
-        colleagues to do the same.</p>
-
-        See all of the <a href="http://365act.com">365 ways to change the world...</a>
-        <?
     } elseif ($microsite == 'london') {
         ?><h2>Tell Londoners &#8220;I&#8217;ll do it, but only if you&#8217;ll help me do it&#8221;</h2>
         <p>In the summer of 2012 the eyes of the world will be on London for a

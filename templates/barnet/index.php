47b55c3ca3b74d2eb495683f8c3b5d16add4ffb3
Dave Whiteland <dave@fury.ukcod.org.uk>
2011-03-17 12:52:33 +0000

    added explicit list of street parties to the Barnet index page too

--- templates/barnet/index.php
+++ templates/barnet/index.php
@@ -49,6 +49,26 @@
         We're pledging to support street parties for the Royal Wedding on 29 April.
         If you can't see a pledge for <i>your</i> street here, why not get involved by <a href="/new/royalwedding">setting one&nbsp;up</a>? 
       </p>
+      <ul>
+          <li><a href="/brownlow-road">Brownlow&nbsp;Road,&nbsp;N3</a></li>
+          <li><a href="/chislehurst-ave">Chislehurst&nbsp;Avenue,&nbsp;N12</a></li>
+          <li><a href="/corringham-road">Corringham&nbsp;Road,&nbsp;NW11</a></li>
+          <li><a href="/dalegreenparty">Dale&nbsp;Green&nbsp;Road,&nbsp;N11</a></li>
+          <li><a href="/falklandroad">Falkland&nbsp;Road,&nbsp;EN5</a></li>
+          <li><a href="/harmandriveparty">Harman&nbsp;Drive,&nbsp;NW2</a></li>
+          <li><a href="/hartlandroad">Hartland&nbsp;Road,&nbsp;N11</a></li>
+          <li><a href="/hertfordclose">Hertford&nbsp;Close,&nbsp;EN4</a></li>
+          <li><a href="/hertford-road">Hertford&nbsp;Road,&nbsp;N2</a></li>
+          <li><a href="/lambertroad">Lambert&nbsp;Road,&nbsp;N12</a></li>
+          <li><a href="/mandevilleroad">Mandeville&nbsp;Road,&nbsp;N14</a></li>
+          <li><a href="/PrincesAvenue">Princes&nbsp;Avenue,&nbsp;N3</a></li>
+          <li><a href="/ravenscroftpark">Ravenscroft&nbsp;Park,&nbsp;EN5</a></li>
+          <li><a href="/rowlandsclose">Rowlands&nbsp;Close,&nbsp;NW7</a></li>
+          <li><a href="/southwayparty">Southway,&nbsp;N20</a></li>
+          <li><a href="/westburyroad">Westbury&nbsp;Road,&nbsp;N12</a></li>
+          <li><a href="/westchester">Westchester&nbsp;Drive,&nbsp;NW4</a></li>
+          <li><a href="/woodvilleroad">Woodville&nbsp;Road,&nbsp;EN5</a></li>
+      </ul>
     </div>
   </div>
 </div>  

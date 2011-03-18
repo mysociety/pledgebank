3a04934f18ac0095e794f251d5d735ff94f77380
Dave Whiteland <dave@fury.ukcod.org.uk>
2011-03-18 10:11:39 +0000

    added protocol to 8PMMovement translate button

--- phplib/pledge.php
+++ phplib/pledge.php
@@ -597,7 +597,7 @@ class Pledge {
 		print '    layout: google.translate.TranslateElement.InlineLayout.SIMPLE';
 		print '  }, "google_translate_element");';
 		print '}';
-		print '</script><script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>';
+		print '</script><script src="http://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>';
 
             }
 

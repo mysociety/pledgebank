7bf2f0de6358aae2456056930dbe3bf532fe5e02
Dave Whiteland <dave@fury.ukcod.org.uk>
2011-11-04 01:39:55 +0000

    initial template tests

--- templates/barnet/contact.php
+++ templates/barnet/contact.php
@@ -31,163 +31,22 @@ $topic = get_http_var('topic');
         print h2(_('Report abusive, suspicious or wrong comment'));
         print p(_("Please let us know exactly what is wrong with the comment, and why you think it should be removed."));
     } elseif ($topic == 'royalwedding') { ?>
-        
-        <h2>Royal Wedding: Street Party in your street</h2>
-        <div class='ms-royalwedding-banner' style="height:200px;"></div>
-        <div id="ms-explicit-party-list">
-          <h3>Our street party pledges:</h3>          
-          <ul>
-              <li><a href="/Alders-Close">Alders&nbsp;Close,&nbsp;HA8</a></li>
-              <li><a href="/asmuns-hill">Asmuns&nbsp;Hill,&nbsp;NW11</a></li>
-              <li><a href="/Athenaeum-Road">Athenaeum&nbsp;Road,&nbsp;N20</a></li>
-              <li><a href="/bevan-road">Bevan&nbsp;Road,&nbsp;EN4</a></li>
-              <li><a href="/Bosworth-Road">Bosworth&nbsp;Road,&nbsp;EN5</a></li>
-              <li><a href="/brownlow-road">Brownlow&nbsp;Road,&nbsp;N3</a></li>
-              <li><a href="/Brookland-Close">Brookland&nbsp;Close,&nbsp;NW11</a></li>
-              <li><a href="/Bryant-Close">Bryant&nbsp;Close,&nbsp;EN5</a></li>
-              <li><a href="/cissbury-road">Cissbury&nbsp;Road</a></li>
-              <li><a href="/corringham-road">Corringham&nbsp;Road,&nbsp;NW11</a></li>
-              <li><a href="/CrewysRoad">Crewys&nbsp;Road,&nbsp;NW2</a></li>
-              <li><a href="/dalegreenparty">Dale&nbsp;Green&nbsp;Road,&nbsp;N11</a></li>
-              <li><a href="/dukesavenue">Dukes&nbsp;Avenue</a></li>
-              <li><a href="/Eastholm">Eastholm,&nbsp;NW11</a></li>
-              <li><a href="/elm-gardens">Elm&nbsp;Gardens,&nbsp;N2</a></li>
-              <li><a href="/exeter-road">Exeter&nbsp;Road,&nbsp;N14</a></li>
-          </ul>
-          <ul>
-              <li><a href="/fairview-way">Fairview&nbsp;Way,&nbsp;HA8</a></li>
-              <li><a href="/falklandroad">Falkland&nbsp;Road,&nbsp;EN5</a></li>
-              <li><a href="/fallowsclose">Fallows&nbsp;Close,&nbsp;N2</a></li>
-              <li><a href="/grove-road">Grove&nbsp;Road,&nbsp;N12</a></li>
-              <li><a href="/harmandriveparty">Harman&nbsp;Drive,&nbsp;NW2</a></li>
-              <li><a href="/harrowes-meade">Harrowes&nbsp;Meade,&nbsp;HA8</a></li>
-              <li><a href="/hartlandroad">Hartland&nbsp;Road,&nbsp;N11</a></li>
-              <li><a href="/hatley-close">Hatley&nbsp;Close,&nbsp;N11</a></li>
-              <li><a href="/hertfordclose">Hertford&nbsp;Close,&nbsp;EN4</a></li>
-              <li><a href="/hertford-road">Hertford&nbsp;Road,&nbsp;N2</a></li>
-              <li><a href="/holly-park">Holly&nbsp;Park</a></li>
-              <li><a href="/lambertroad">Lambert&nbsp;Road,&nbsp;N12</a></li>
-              <li><a href="/Leicester-Road">Leicester&nbsp;Road,&nbsp;N2</a></li>
-              <li><a href="/Limes-Ave">Limes&nbsp;Avenue</a></li>
-              <li><a href="/marion-road">Marion&nbsp;Road,&nbsp;NW7</a></li>
-              <li><a href="/myddleton-park">Myddleton&nbsp;Park,&nbsp;N20</a></li>
-          </ul>
-          <ul>
-              <li><a href="/Norwich-Walk">Norwich&nbsp;Walk&nbsp;HA8</a></li>
-              <li><a href="/Ossulton-Way">Ossulton&nbsp;Way,&nbsp;N2</a></li>
-              <li><a href="/Park-Avenue">Park&nbsp;Avenue,&nbsp;N3</a></li>
-              <li><a href="/prospect-road">Prospect&nbsp;Road&nbsp;&amp;&nbsp;Place,&nbsp;NW2</a></li>
-              <li><a href="/quinta-green">Quinta&nbsp;Green,&nbsp;Mays&nbsp;Lane</a></li>
-              <li><a href="/ravenscroftpark">Ravenscroft&nbsp;Park,&nbsp;EN5</a></li>
-              <li><a href="/rowlandsclose">Rowlands&nbsp;Close,&nbsp;NW7</a></li>
-              <li><a href="/sandwick-close">Sandwick&nbsp;Close,&nbsp;NW7</a></li>
-              <li><a href="/Sellers-Hall">Sellers&nbsp;Hall&nbsp;Close,&nbsp;N3</a></li>
-              <li><a href="/southwayparty">Southway,&nbsp;N20</a></li>
-              <li><a href="/Stanaley-Road">Stanaley&nbsp;Road,&nbsp;N2</a></li>
-              <li><a href="/Victoria-Rd">Victoria&nbsp;Road</a></li>
-              <li><a href="/westburyroad">Westbury&nbsp;Road,&nbsp;N12</a></li>
-              <li><a href="/woodlands-avenue">Woodlands&nbsp;Ave,&nbsp;N3</a></li>
-              <li><a href="/woodvilleroad">Woodville&nbsp;Road,&nbsp;EN5</a></li>              
-          </ul>
-          <div style="width:100%;clear:both;height:1px"></div>
-        </div>
-        <div style="font-size:1.126em;padding-bottom:0.25em;">
-            <p>
-                 The Royal Wedding of HRH Prince William and Kate Middleton took place on 29 April 2011. 
-                 It was a wonderful excuse for a street party &mdash; not only is a street party a fun way for residents 
-                 to celebrate such an event, it&rsquo;s also a great way to get to know your neighbours better.               
-            </p>
-        </div>
-        <div style="float:right; width:45%;margin-left:1em;" class="pb-barnet-breakout">
-          <h3>
-              What you will need to do:
-          </h3>
-          <p>
-              Sorry &mdash; it&rsquo;s too late to notify us of a <i>new</i> Royal Wedding street party now.
-              But you could sign up for a <a href="/new/thebiglunch">Big Lunch street party</a> for the <strong>5&nbsp;June</strong> instead!
-          </p>
-          <h3>
-            What we pledged: 
-          </h3>
-          <ul>
-            <li>
-              Barnet Council pledged support street parties on the day of the Royal Wedding, including arranging insurance free of charge, <strong>if at least three households in a street</strong> signed up to holding an event.
-            </li>
-          </ul>
-        </div>
-        <h3>
-            Note: Deadline for applications is now closed
-        </h3> 
+
+      <!-- including royalwedding.php -->
+      <? include 'types/royalwedding.php' ?>       
         
     <? } else { ?>
        
        <? if ($topic == 'thebiglunch') { ?>
-      
-          <!-- =========================== the big lunch =================================== -->
-        
-          <h2>The Big Lunch: a one day get-together for neighbours</h2>
-          <div class='ms-thebiglunch-banner' style="height:200px;"></div>
-          <div id="ms-explicit-party-list">
-            <h3>Sign up to a Big Lunch street party pledge:</h3>          
-            <ul>
-                <li><a href="/corringham-rd-bl">Corringham&nbsp;Road,&nbsp;NW11</a></li>
-                <li><a href="/heath-view-bl">Heath&nbsp;View,&nbsp;N2</a></li>
-                <li><a href="/jackson-rd-bl">Jackson&nbsp;Road,&nbsp;EN4</a></li>
-                <li><a href="/park-rd-bl">Park&nbsp;Road,&nbsp;EN5</a></li>
-                <li><a href="/salcombe-gdns-bl">Salcombe&nbsp;Gardens,&nbsp;NW7</a></li>
-            </ul>
-            <div style="width:100%;clear:both;height:1px"></div>
-          </div>
-          <div style="font-size:1.126em;padding-bottom:0.25em;">
-             <a href="http://www.barnet.gov.uk/biglunch" title="about The Big Lunch"><img 
-                alt="The Big Lunch logo" src="/microsites/barnet/the_big_lunch_logo_212_x_90.png" 
-                style="float:right;width:212px;height:90px;border:none;margin: 0 1em 1em 1em;"/></a>
-            <p>
-                <a href="http://www.barnet.gov.uk/biglunch">The Big Lunch</a> is a very simple idea from the Eden Project. 
-                The aim is to get as many people as possible across the whole of the UK to have lunch with their
-                neighbours in a simple act of community, friendship and fun. This year it&rsquo;s happening on 
-                <strong>Sunday&nbsp;5&nbsp;June</strong>.
-            </p>
-            <p>
-                A Big Lunch can be anything from a few neighbours getting together in the garden or on the street, 
-                to a full blown party with food, music and decoration that quite literally stops the traffic.              
-                If it is on the street, you will need to get Barnet Council&rsquo;s permission to make sure it is OK 
-                for you to close the road and sort out your insurance. To do this, we would like 3 households on each 
-                street to sign up to a pledge.
-            </p>
-          </div>
-          <div style="width:100%;clear:both;height:1px"></div>
-          <div style="float:right; width:45%;margin-left:1em;" class="pb-barnet-breakout">
-            <h3>
-                What you will need to do:
-            </h3>
-            <ul>
-              <li>
-                  Read our guide to <a href="http://www.barnet.gov.uk/biglunch">holding a Big Lunch street party</a>.
-              </li>
-              <!-- <li>
-                Read the <a href="http://www.barnet.gov.uk/royal-wedding-insurance.pdf">policy wording of the public liability insurance cover</a> arranged by the Council.
-              </li>-->
-              <li>
-                Fill in a <a href="http://www.barnet.gov.uk/Big-Lunch-A4-Form-web.pdf">The Big Lunch street party application form</a> telling us about the road or section of road you would like to close on the day.
-              </li>            
-            </ul>
-            <h3>
-              What we're pledging: 
-            </h3>
-            <ul>
-              <li>
-                Barnet Council will support street parties on the day of The Big Lunch, including arranging insurance free of charge, if at least three households in a street sign up to get involved. 
-              </li>
-            </ul>
-          </div>
-          <h3>
-              Can&rsquo;t see your road?  Start a new pledge!
-          </h3>            
-          <p>
-              If there&rsquo;s not already a pledge for your road, simply start one. 
-              Ideally, you&rsquo;ll need to get a minimum of 3&nbsp;households involved. 
-          </p>
+         
+         <!-- including thebiglunch.php -->
+         <? include 'types/thebiglunch.php' ?>
+         
+      <? } elseif ($topic == 'star_wars') { ?>
+
+         <!-- including star_wars.php -->
+        <? include 'types/star_wars.php' ?>
+
        <? } else { 
 
           print h2("Suggest a pledge");
@@ -196,6 +55,9 @@ $topic = get_http_var('topic');
           print "<p>What would you like to get done? </p>";
 
         }
+        
+        
+        
         print "<p>";
         $contact_email = str_replace('@', '&#64;', OPTION_CONTACT_EMAIL);
         printf(_('If you prefer, you can email %s instead of using the form.'), '<a href="mailto:' . $contact_email . '">' . $contact_email . '</a>');
---
+++ templates/barnet/types/royalwedding.php
@@ -0,0 +1,86 @@
+<h2>Royal Wedding: Street Party in your street</h2>
+<div class='ms-royalwedding-banner' style="height:200px;"></div>
+<div id="ms-explicit-party-list">
+  <h3>Our street party pledges:</h3>          
+  <ul>
+      <li><a href="/Alders-Close">Alders&nbsp;Close,&nbsp;HA8</a></li>
+      <li><a href="/asmuns-hill">Asmuns&nbsp;Hill,&nbsp;NW11</a></li>
+      <li><a href="/Athenaeum-Road">Athenaeum&nbsp;Road,&nbsp;N20</a></li>
+      <li><a href="/bevan-road">Bevan&nbsp;Road,&nbsp;EN4</a></li>
+      <li><a href="/Bosworth-Road">Bosworth&nbsp;Road,&nbsp;EN5</a></li>
+      <li><a href="/brownlow-road">Brownlow&nbsp;Road,&nbsp;N3</a></li>
+      <li><a href="/Brookland-Close">Brookland&nbsp;Close,&nbsp;NW11</a></li>
+      <li><a href="/Bryant-Close">Bryant&nbsp;Close,&nbsp;EN5</a></li>
+      <li><a href="/cissbury-road">Cissbury&nbsp;Road</a></li>
+      <li><a href="/corringham-road">Corringham&nbsp;Road,&nbsp;NW11</a></li>
+      <li><a href="/CrewysRoad">Crewys&nbsp;Road,&nbsp;NW2</a></li>
+      <li><a href="/dalegreenparty">Dale&nbsp;Green&nbsp;Road,&nbsp;N11</a></li>
+      <li><a href="/dukesavenue">Dukes&nbsp;Avenue</a></li>
+      <li><a href="/Eastholm">Eastholm,&nbsp;NW11</a></li>
+      <li><a href="/elm-gardens">Elm&nbsp;Gardens,&nbsp;N2</a></li>
+      <li><a href="/exeter-road">Exeter&nbsp;Road,&nbsp;N14</a></li>
+  </ul>
+  <ul>
+      <li><a href="/fairview-way">Fairview&nbsp;Way,&nbsp;HA8</a></li>
+      <li><a href="/falklandroad">Falkland&nbsp;Road,&nbsp;EN5</a></li>
+      <li><a href="/fallowsclose">Fallows&nbsp;Close,&nbsp;N2</a></li>
+      <li><a href="/grove-road">Grove&nbsp;Road,&nbsp;N12</a></li>
+      <li><a href="/harmandriveparty">Harman&nbsp;Drive,&nbsp;NW2</a></li>
+      <li><a href="/harrowes-meade">Harrowes&nbsp;Meade,&nbsp;HA8</a></li>
+      <li><a href="/hartlandroad">Hartland&nbsp;Road,&nbsp;N11</a></li>
+      <li><a href="/hatley-close">Hatley&nbsp;Close,&nbsp;N11</a></li>
+      <li><a href="/hertfordclose">Hertford&nbsp;Close,&nbsp;EN4</a></li>
+      <li><a href="/hertford-road">Hertford&nbsp;Road,&nbsp;N2</a></li>
+      <li><a href="/holly-park">Holly&nbsp;Park</a></li>
+      <li><a href="/lambertroad">Lambert&nbsp;Road,&nbsp;N12</a></li>
+      <li><a href="/Leicester-Road">Leicester&nbsp;Road,&nbsp;N2</a></li>
+      <li><a href="/Limes-Ave">Limes&nbsp;Avenue</a></li>
+      <li><a href="/marion-road">Marion&nbsp;Road,&nbsp;NW7</a></li>
+      <li><a href="/myddleton-park">Myddleton&nbsp;Park,&nbsp;N20</a></li>
+  </ul>
+  <ul>
+      <li><a href="/Norwich-Walk">Norwich&nbsp;Walk&nbsp;HA8</a></li>
+      <li><a href="/Ossulton-Way">Ossulton&nbsp;Way,&nbsp;N2</a></li>
+      <li><a href="/Park-Avenue">Park&nbsp;Avenue,&nbsp;N3</a></li>
+      <li><a href="/prospect-road">Prospect&nbsp;Road&nbsp;&amp;&nbsp;Place,&nbsp;NW2</a></li>
+      <li><a href="/quinta-green">Quinta&nbsp;Green,&nbsp;Mays&nbsp;Lane</a></li>
+      <li><a href="/ravenscroftpark">Ravenscroft&nbsp;Park,&nbsp;EN5</a></li>
+      <li><a href="/rowlandsclose">Rowlands&nbsp;Close,&nbsp;NW7</a></li>
+      <li><a href="/sandwick-close">Sandwick&nbsp;Close,&nbsp;NW7</a></li>
+      <li><a href="/Sellers-Hall">Sellers&nbsp;Hall&nbsp;Close,&nbsp;N3</a></li>
+      <li><a href="/southwayparty">Southway,&nbsp;N20</a></li>
+      <li><a href="/Stanaley-Road">Stanaley&nbsp;Road,&nbsp;N2</a></li>
+      <li><a href="/Victoria-Rd">Victoria&nbsp;Road</a></li>
+      <li><a href="/westburyroad">Westbury&nbsp;Road,&nbsp;N12</a></li>
+      <li><a href="/woodlands-avenue">Woodlands&nbsp;Ave,&nbsp;N3</a></li>
+      <li><a href="/woodvilleroad">Woodville&nbsp;Road,&nbsp;EN5</a></li>              
+  </ul>
+  <div style="width:100%;clear:both;height:1px"></div>
+</div>
+<div style="font-size:1.126em;padding-bottom:0.25em;">
+    <p>
+         The Royal Wedding of HRH Prince William and Kate Middleton took place on 29 April 2011. 
+         It was a wonderful excuse for a street party &mdash; not only is a street party a fun way for residents 
+         to celebrate such an event, it&rsquo;s also a great way to get to know your neighbours better.               
+    </p>
+</div>
+<div style="float:right; width:45%;margin-left:1em;" class="pb-barnet-breakout">
+  <h3>
+      What you will need to do:
+  </h3>
+  <p>
+      Sorry &mdash; it&rsquo;s too late to notify us of a <i>new</i> Royal Wedding street party now.
+      But you could sign up for a <a href="/new/thebiglunch">Big Lunch street party</a> for the <strong>5&nbsp;June</strong> instead!
+  </p>
+  <h3>
+    What we pledged: 
+  </h3>
+  <ul>
+    <li>
+      Barnet Council pledged support street parties on the day of the Royal Wedding, including arranging insurance free of charge, <strong>if at least three households in a street</strong> signed up to holding an event.
+    </li>
+  </ul>
+</div>
+<h3>
+    Note: Deadline for applications is now closed
+</h3> 

bc8e150cbc744b18eb8937697a14d69ec35658f5
Dave Whiteland <dave@fury.ukcod.org.uk>
2011-11-13 17:10:30 +0000

    text approved by Barnet

--- templates/barnet/types/adoptastreet.php
+++ templates/barnet/types/adoptastreet.php
@@ -1,6 +1,6 @@
 <!-- =========================== adopt-a-street =================================== -->
 
- <h2>Adopt a Street</h2>
+ <h2>Adopt-a-Street</h2>
  <div class='ms-adoptastreet-banner' style="height:200px;"></div>
  <div id="ms-explicit-party-list">
    <h3>Sign up to an Adopt-a-Street pledge:</h3>
@@ -8,20 +8,20 @@
    <div style="width:100%;clear:both;height:1px"></div>
  </div>
  <div style="font-size:1.126em;padding-bottom:0.25em;">
-   <p>
+   <p style="font-weight:bold;">
+     'Adopt-a-street' encourages local volunteers to improve and care for their area by carrying out
+     extra work which will make their neighbourhoods look more attractive, including planting, 
+     gardening and litter sweeps.
+  </p>
+  <p>
      When residents sign up to adopt their street they will receive training from council officers 
-     and be provided with appropriate equipment such as litter pickers, shovels and rubbish sacks.
-     Prior to the winter months, 'adopted' streets will also receive a delivery of grit and 
-     equipment to remove snow and spread grit on their pavements.
+     and be provided with appropriate equipment such as litter pickers, shovels and rubbish sacks. 
+     Prior to the winter months, 'adopted' streets will also receive a delivery of grit and equipment 
+     to remove snow and spread grit on their pavements. 
    </p>
-   <p>
-     After a highly successful trial, Barnet Council is offering similar support to an initial 
-     10 streets who sign up to the scheme.
-     To do this, we would like 6 households on each street to sign up to a pledge.
-   </p>
-   <p>
-     <a href="http://www.barnet.gov.uk/press_releases.htm?id=2401">Read more about the scheme.</a>
-    </p>
+   <ul>
+     <li><a href="http://www.barnet.gov.uk/press_releases.htm?id=2401">Read more about the pilot scheme.</a></li>
+    </ul>
  </div>
  <div style="width:100%;clear:both;height:1px"></div>
  <div style="float:right; width:45%;margin-left:1em;" class="pb-barnet-breakout">
@@ -30,10 +30,13 @@
    </h3>
    <ul>
      <li>
-       Lorem ipsum dolor sit amet, consectetur adipisicing elit
+       Request to adopt your street &mdash; unless there is a pledge already set up for your street.
      </li>
      <li>
-       velit esse cillum dolore eu fugiat nulla pariatur
+       Encourage your neighbours to sign the street pledge.
+    </li>
+    <li>
+      Once your pledge target is met then you will get training and equipment.
     </li>
    </ul>
    <h3>
@@ -47,9 +50,8 @@
    </ul>
  </div>
  <h3>
-     Can&rsquo;t see your road?  Start a new pledge!
- </h3>            
+   Can’t see your road? Start a new pledge! 
+ </h3>
  <p>
-     If there&rsquo;s not already a pledge for your road, simply start one. 
-     Ideally, you&rsquo;ll need to get a minimum of 6&nbsp;households involved. 
+   If there’s not already a pledge for your road, simply start one. Ideally, you’ll need to get a minimum of 6 households involved.
  </p>

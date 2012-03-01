f766c31cde1e487892c635a32da7fbed232273b3
Dave Whiteland <dave@mysociety.org>
2012-03-01 17:15:01 +0000

    updated copy on closed gritting pledge templates in Barnet

--- templates/barnet/types/grit_my_school.php
+++ templates/barnet/types/grit_my_school.php
@@ -3,78 +3,36 @@
 <h2>Help clear the snow in your neighbourhood this winter</h2>
 <div class='ms-grit_my_school-banner' style="height:200px;"></div>
 <div id="ms-explicit-party-list">
-  <h3>Sign up to a pledge to clear snow from your school:</h3>
+  <h3>Pledges for clearing snow from schools:</h3>
   <?  print pledge_get_ul_list_by_type('grit_my_school', 3, 3, "pledge_type_auto_width_column"); ?>
   <div style="width:100%;clear:both;height:1px"></div>
 </div>
 <div style="font-size:1.126em;padding-bottom:0.25em;">
     <p style="font-weight:bold;">
-        Barnet Council will support volunteers who wish to clear snow by offering grit and spreading equipment. 
-        Four volunteers must sign up to the pledge (one of whom will be the community grit keeper).  
-    </p>
-    <div class="pb-barnet-breakout" style="position:relative;padding-top: 1em;">
-       <p class="corner-label">
-         Please note:
-       </p>
-       <h4>
-         Storing the grit supplies
-       </h4>
-       <p>
-         If you agree to become a Community Grit Keeper you will need space in your property to store a one ton bag of grit (approximately 1m<span class="superscript">3</span> in size). If you do not have the appropriate storage space, we will put you in touch with the other volunteers that have signed up to your pledge so you can check whether they can store the grit supplies.
-       </p>
-       <h4>
-         Sharing your contact details with other volunteers
-       </h4>
-       <p>
-         If you agree to become a Community Grit Keeper or volunteer, we will automatically share your contact details that with other participants in your school.
-       </p>
-     </div>
-    
+        Barnet Council supported volunteers who wished to clear snow by offering grit and spreading equipment. 
+        Four volunteers needed sign up to a pledge for it to succeed.  
+    </p>    
     
     <p>
-        The council will provide advice and insurance cover.
-    </p>
-    <p> 
-        Schools have the chance to take part and get parents and volunteers 
+        The council pledged to provide advice and insurance cover.
+        Schools had the chance to take part and get parents and volunteers 
         to help clear access to the school. 
   </p>
-  <p>
-    If you would like to volunteer, you need to sign up to a pledge. If you can’t see your school, 
-    then you need to start a new pledge.
-  </p>
 </div>
-<div style="width:100%;clear:both;height:1px"></div>
-<div style="float:right; width:45%;margin-left:1em;position:relative;" class="pb-barnet-breakout">
-  <h3>
-     What you need to do: Sign up to a pledge!
-  </h3>
-  <p>
-    Check to see if your school is shown at the top of this page &mdash; click on it and sign up!
-  <h3>
-  Can’t see your school? Start a new pledge! 
-  </h3>
-  <p>
-  If there’s not already a pledge for your school, simply start one. 
-  You will need to get a minimum of 4 volunteers involved for the pledge to be successful.
+
+
+<div class="pb-barnet-breakout" style="position:relative;padding-top: 1em;">
+  <p class="corner-label">
+    Closed:
   </p>
+  <h4>
+    The school gritting pledges are now closed.
+  </h4>
 </div>
+
 <ul style="font-size:1.126em;margin-bottom:2em;">
   <li>
-      looking for your <strong>street</strong>? We're also running similar pledges for
+      looking for your <strong>street</strong>? We also ran similar pledges for
       <a href="/type/grit_my_street">clearing snow from streets in Barnet</a>!
   </li>
-</ul>
-<? print_contact_form($name, $email, $topic, true) ?>
-<ul>
-    <li>
-        Fill in the request form
-    </li>
-    <li>
-      Barnet council will use this information to check the general practicalities, 
-      grit supplies and set up a pledge. We will email you to notify this is successful 
-      and send you a link to the pledge
-    </li>
-    <li>
-      After that, it’s up to you to spread the word in your community and get people to sign the pledge.
-    </li>
 </ul>
--- templates/barnet/types/grit_my_street.php
+++ templates/barnet/types/grit_my_street.php
@@ -3,7 +3,7 @@
 <h2>Help clear the snow in your neighbourhood this winter</h2>
 <div class='ms-grit_my_street-banner' style="height:200px;"></div>
 <div id="ms-explicit-party-list">
-  <h3>Sign up to a pledge to clear snow from your street:</h3>
+  <h3>Pledges for clearing snow from streets:</h3>
   
     <ul class='pledge_type_auto_width_column'>
         <li><a href='/asmuns-hill-grit'>Asmuns Hill, NW11,</a></li>
@@ -30,8 +30,8 @@
 </div>
 <div style="font-size:1.126em;padding-bottom:0.25em;">
   <p style="font-weight:bold;">
-      Barnet Council will support residents who wish to clear snow by offering grit and spreading equipment. 
-      Four residents must sign up to the pledge (one of whom will be the community grit keeper).  
+      Barnet Council supported residents who wished to clear snow by offering grit and spreading equipment. 
+      Four residents needed to sign up to a pledge for it to succeed.  
   </p>
   
   <div class="pb-barnet-breakout" style="position:relative;padding-top: 1em;">
@@ -45,53 +45,15 @@
     <p>
     Therefore we will no longer be processing any new requests for a “grit my street” pledge, or any current pledges that are yet to be fulfilled.
     </p>
-    <p>
-    We will continue to review this situation in light of ongoing weather.
-    </p>
-  </div>
-  <div class="pb-barnet-breakout" style="position:relative;padding-top: 1em;">
-      <p class="corner-label">
-        Please note:
-      </p>
-    <h4>
-      Storing the grit supplies
-    </h4>
-    <p>
-      If you agree to become a Community Grit Keeper you will need space on your property to store a one ton bag of grit (approximately 1m<span class="superscript">3</span> in size). If you do not have the appropriate storage space, we will put you in touch with the other volunteers that have signed up to your pledge so you can check whether they can store the grit supplies.
-    </p>
-    <h4>
-      Sharing your contact details with other volunteers
-    </h4>
-    <p>
-      If you agree to become a Community Grit Keeper or volunteer, we will automatically share your contact details  with other participants in your street.
-    </p>
   </div>
   
   <p>
-      The council will provide advice and insurance cover.
-  </p>
-  <p>
-    If you would like to volunteer, you need to sign up to a pledge. If you can’t see your street, 
-    then you need to start a new pledge.
-  </p>
-</div>
-<div style="width:100%;clear:both;height:1px"></div>
-<div style="float:right; width:45%;margin-left:1em;position:relative;" class="pb-barnet-breakout">
-  <h3>
-     What you need to do: Sign up to a pledge!
-  </h3>
-  <p>
-    Check to see if your street is shown at the top of this page &mdash; click on it and sign up!
-  <h3>
-  Can’t see your street?
-  </h3>
-  <p>
-  If there’s not already a pledge for your street, sorry &mdash; we’re not running any more “grit my street” pledges at the moment.
+      The council provided advice and insurance cover.
   </p>
 </div>
 <ul style="font-size:1.126em;margin-bottom:2em;">
   <li>
-      looking for your <strong>school</strong>? We're also running similar pledges for
+      looking for your <strong>school</strong>? We also ran similar pledges for
       <a href="/type/grit_my_school">clearing snow from schools in Barnet</a>!
   </li>
 </ul>

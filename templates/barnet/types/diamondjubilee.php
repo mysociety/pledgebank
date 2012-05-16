843bad705964519b9b8a820227f970f16361fac1
Dave Whiteland <dave@mysociety.org>
2012-05-16 10:39:14 +0100

    another copy change for Barnet diamond jubilee street party pledges

--- templates/barnet/types/diamondjubilee.php
+++ templates/barnet/types/diamondjubilee.php
@@ -12,6 +12,25 @@
   <div style="width:100%;clear:both;height:1px"></div>
 
 </div>
+<div style="position:relative;margin-top:1em;" class="pb-barnet-breakout">
+  <h3>
+    Diamond Jubilee street party pledges are now closed
+  </h3>
+  <p>
+	Sorry &mdash; applications for Diamond Jubilee street party pledges are now closed, and
+	the sign-up deadlines have passed.
+  </p>
+<p>
+    However, if you're interested in arranging a party in the <strong>park</strong> rather than your street,
+    there is still a little time left! Please see the 
+	<a href="/type/diamondparkparty">Diamond Jubilee parties in the park</a> page instead.
+  </p>
+  <p>
+	Alternatively, you could start planning your <a href="/type/olympics2012">2012 Olympics street party</a>
+	instead!
+  </p>
+</div>
+
 <div style="ms-general-box">
   <img src="/microsites/barnet/diamond_jubilee_150_x_200.png" alt="2012 Diamond Jubilee logo" style="width:150px;height:200px;margin:0 0 8px 1em; float:right;" />
   <p style="font-weight:bold;margin-top:1em;">
@@ -24,47 +43,22 @@
     It is a wonderful excuse for a street party &mdash; not only is a street party a fun way for residents 
     to celebrate such an event, it&rsquo;s also a great way to get to know your neighbours better.
   </p>
-  <div class="pb-barnet-breakout" style="margin-right:168px; position:relative;padding-top: 1em;">
-    <p class="corner-label">
-      Please note:
-    </p>
-    <h4>
-      Sharing your contact details with other volunteers
-    </h4>
-    <p>
-      If you agree to become a volunteer, we will automatically share your contact details with other participants in your street.
-    </p>
-  </div>
   <p>
     The council will provide advice and insurance cover.
   </p>
 </div>
-<div style="width:100%;clear:both;height:1px"></div>
-<div style="float:right; width:45%;margin-left:1em;position:relative;" class="pb-barnet-breakout">
-  <h3>
-    What you need to do: Sign up to a pledge!
-  </h3>
-  <p>
-    Check to see if your street is shown at the top of this page &mdash; click on it and sign up!
-    You will need to get a minimum of 3 households involved for the pledge to be successful.
+
+<div class="pb-barnet-breakout" style="margin-right:168px; position:relative;padding-top: 1em;">
+  <p class="corner-label">
+    Please note:
   </p>
-  <h3>
-    Can&rsquo;t see your street? 
-  </h3>
+  <h4>
+    Sharing your contact details with other volunteers
+  </h4>
   <p>
-	Sorry &mdash; applications for Diamond Jubilee street party pledges are now closed.
-	You could start planning a <a href="/type/olympics2012">2012 Olympics street party</a>
-	instead though!
+    If you agree to become a volunteer, we will automatically share your contact details with other participants in your street.
   </p>
 </div>
-<p>
-    If you're interested in arranging a party in the <strong>park</strong> rather than your street,
-    please see the <a href="/type/diamondparkparty">Diamond Jubilee parties in the park</a> page instead.
-  </p>
-
-<p><strong>
-	Applications for Diamond Jubilee street parties are now closed!
-</strong></p>
 
 <p>
   Remember that it&rsquo;s up to you to spread the word to your neighbours to get them to sign your pledge and get involved!

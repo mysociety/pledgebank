0d395b3ba2f439ccdd79dec6d836a198959dc784
Hakim Cassimally <hakim@mysociety.org>
2014-05-06 16:47:27 +0100

    RWBM microsite settings for location/privacy

--- phplib/microsites.php
+++ phplib/microsites.php
@@ -348,6 +348,9 @@ function microsites_default_location() {
     if ($microsite == 'barnet'){
         return array('local'=> 1, 'place'=> 'Barnet', 'country' => 'GB', 'state' => '', 'gaze_place' => '', 'postcode' => '', 'places' => null);
     }
+    if ($microsite == 'rbwm'){
+        return array('local'=> 1, 'place'=> 'Windsor', 'country' => 'GB', 'state' => '', 'gaze_place' => '', 'postcode' => '', 'places' => null);
+    }
     return array(); /* empty array, not null */
 }
 
@@ -460,6 +463,8 @@ greater publicity and a greater chance of succeeding.');
 # Features
 
 function microsites_location_allowed() {
+    global $microsite;
+    if ($microsite == 'rbwm') return false;
     return true;
 }
 
@@ -523,6 +528,7 @@ function microsite_conditional_firstperson($name, $pledge_microsite) {
  * Returns whether private pledges are offered in new pledge dialog. */
 function microsites_private_allowed() {
     global $microsite;
+    if ($microsite == 'rbwm') return false;
     if ($microsite == 'barnet') return false;
     return true;
 }
@@ -532,6 +538,7 @@ function microsites_private_allowed() {
 function microsites_categories_allowed() {
     global $microsite;
     if ($microsite == 'barnet') return false;
+    if ($microsite == 'rbwm') return false;
     return true;
 }
 
@@ -1140,6 +1147,7 @@ function microsites_new_pledges_preview_extras($data) {
 function microsites_change_microsite_allowed() {
     global $microsite;
     if ($microsite == 'barnet') return false;
+    if ($microsite == 'rbwm') return false;
     return true;
 }
 
@@ -1150,6 +1158,7 @@ function microsites_change_microsite_allowed() {
 function microsites_show_translate_blurb() {
     global $microsite;
     if ($microsite == 'barnet') return false;
+    if ($microsite == 'rbwm') return false;
     return true;
 }
 
@@ -1160,6 +1169,7 @@ function microsites_show_translate_blurb() {
 function microsites_show_alert_advert() {
     global $microsite;
     if ($microsite == 'barnet') return false;
+    if ($microsite == 'rbwm') return false;
     return true;
 } 
 
@@ -1168,7 +1178,8 @@ function microsites_show_alert_advert() {
  */
 function microsites_show_area($pledge_microsite) {
     global $microsite;
-    if ($microsite == 'barnet' || $pledge_microsite == 'barnet') return false;
+    if ($microsite == 'barnet') return false;
+    if ($microsite == 'rbwm') return false;
     return true;    
 }
 
@@ -1206,6 +1217,7 @@ function microsites_no_target() {
 function microsites_has_sms() {
     global $microsite;
     if ($microsite == 'barnet') return false;
+    if ($microsite == 'rbwm') return false;
     return true;
 }
 
